c0e089dbd4eaec402d1eb1a674c35214ad4628b5
fishery <i@ovo.so>
2020-01-28 01:12:09 -0600

    added a media_downloader extracted from utils

---
+++ media_downloader.py
@@ -0,0 +1,21 @@
+import youtube_dl
+from pathlib import Path
+from requests import get
+from urllib.parse import urlparse
+
+def video_downloader(video_url : str, download_location : str):
+    download_path = str(Path(download_location, "%(id)s.%(ext)s"))
+    ydl_opts = {
+        "outtmpl": download_path,
+        "quiet": True,
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url, ])
+
+def photo_downloader(photo_url : str, download_location : str):
+    url_obj = urlparse(photo_url)
+    file_name = url_obj.path.replace("/media/", "")
+    path = str(Path(download_location, file_name))
+    if not Path(path).is_file():
+        with open(path, "wb") as file:
+            file.write(get(photo_url).content)
