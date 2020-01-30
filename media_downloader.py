efa97d76ce288f26b439f15dd9600e7167cf712b
fishery <i@ovo.so>
2020-01-29 20:17:25 -0600

    renamed func name in media downloader

--- media_downloader.py
+++ media_downloader.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from requests import get
 from urllib.parse import urlparse
 
-def video_downloader(video_url : str, download_location : str):
+def video(video_url : str, download_location : str):
     download_path = str(Path(download_location, "%(id)s.%(ext)s"))
     ydl_opts = {
         "outtmpl": download_path,
@@ -12,7 +12,7 @@ def video_downloader(video_url : str, download_location : str):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url, ])
 
-def photo_downloader(photo_url : str, download_location : str):
+def photo(photo_url : str, download_location : str):
     url_obj = urlparse(photo_url)
     file_name = url_obj.path.replace("/media/", "")
     path = str(Path(download_location, file_name))
