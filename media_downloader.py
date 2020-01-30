840b291b80c8d6987fe735ccda5758ac1498895a
fishery <i@ovo.so>
2020-01-29 20:54:54 -0600

    fixed file extension in photo downloader

--- media_downloader.py
+++ media_downloader.py
@@ -15,7 +15,7 @@ def video(video_url : str, download_location : str):
         ydl.download([video_url, ])
 
 def photo(photo_name : str, download_location : str):
-    path = str(Path(download_location, photo_name))
+    path = str(Path(download_location, photo_name + ".jpg"))
     if not Path(path).is_file():
         with open(path, "wb") as file:
-            file.write(get(PHOTO_BASE_URL + photo_name).content)
+            file.write(get(PHOTO_BASE_URL + photo_name + ".jpg").content)
