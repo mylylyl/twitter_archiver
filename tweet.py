1b4666d642f0afe22beb5ddada33045fb214bfb3
fishery <i@ovo.so>
2020-02-01 18:32:10 -0600

    changed to use db name with directory

--- tweet.py
+++ tweet.py
@@ -5,20 +5,9 @@ import sqlite3
 class tweet(object):
     def __init__(self, username : str):
         self.username = username
-        self.dbname = self.username + ".db"
+        self.dbname = "database\\%s.db" % self.username
 
     def archive(self):
-        # move the db to cwd if exists
-        if path.exists("db\\" + self.dbname) and not path.exists(self.dbname):
-            rename("db\\" + self.dbname, self.dbname)
-        # store tweets
-        self.store_tweets()
-        # move to db folder
-        if path.exists("db\\" + self.dbname):
-            remove("db\\" + self.dbname)
-        rename(self.dbname, "db\\" + self.dbname)
-
-    def store_tweets(self):
         # store to db
         c = twint.Config()
         c.Username = self.username
