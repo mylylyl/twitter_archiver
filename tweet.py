4734af1c894007957e3ba5dc6b271351aa0c3ef5
fishery <i@ovo.so>
2020-02-01 18:19:55 -0600

    added tweet.py for tweets related func

---
+++ tweet.py
@@ -0,0 +1,36 @@
+from os import path, mkdir, rename, remove
+import twint
+import sqlite3
+
+class media(object):
+    def __init__(self, username : str):
+        self.username = username
+        self.dbname = self.username + ".db"
+
+    def archive(self):
+        # move the db to cwd if exists
+        if path.exists("db\\" + self.dbname) and not path.exists(self.dbname):
+            rename("db\\" + self.dbname, self.dbname)
+        # store tweets
+        self.store_tweets()
+        # move to db folder
+        if path.exists("db\\" + self.dbname):
+            remove("db\\" + self.dbname)
+        rename(self.dbname, "db\\" + self.dbname)
+
+    def store_tweets(self):
+        # store to db
+        c = twint.Config()
+        c.Username = self.username
+        c.User_full = True
+        c.Hide_output = True
+        c.Database = self.dbname
+        
+        try:
+            twint.run.Search(c)
+        except sqlite3.OperationalError:
+            print("DB Error")
+        except:
+            print("Error")
+        else:
+            print("[√] finished downloading all tweets from %s" % self.username)
