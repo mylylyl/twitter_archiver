10b3a55675947b0781d2b8ee28fbaa3e62f1b7e3
fishery <i@ovo.so>
2020-02-02 13:29:02 -0600

    added asyncio to use twint in multithread environment

--- tweet.py
+++ tweet.py
@@ -1,6 +1,9 @@
 from os import path, mkdir, rename, remove
-import twint
+import sys
 import sqlite3
+import asyncio
+
+import twint
 
 class tweet(object):
     def __init__(self, username : str):
@@ -8,6 +11,9 @@ class tweet(object):
         self.dbname = "%s\\tweet.db" % self.username
 
     def archive(self):
+        # use twint in multithread env
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        
         # store to db
         c = twint.Config()
         c.Username = self.username
@@ -18,8 +24,12 @@ class tweet(object):
         try:
             twint.run.Search(c)
         except sqlite3.OperationalError:
-            print("DB Error")
+            print("[x] database error")
+            sys.exit(1)
+        except RuntimeError:
+            print("[x] runtime error: ", sys.exc_info())
         except:
-            print("Error")
+            print("[x] unexpected error: ", sys.exc_info())
+            sys.exit(1)
         else:
             print("[√] finished downloading all tweets from %s" % self.username)
