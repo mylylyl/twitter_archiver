04cd50712e0c5b8fb527e874650acbcb7508e260
fishery <i@ovo.so>
2020-02-02 13:30:11 -0600

    changed account to initiate tweet first then media

--- account.py
+++ account.py
@@ -1,12 +1,18 @@
+from os import path, mkdir
+
 from tweet import tweet
 from media import media
 
 class account:
     def __init__(self, username : str):
         self.username = username
-        self.tweet_manager = tweet(self.username)
-        self.media_manager = media(self.username)
+
+        # check and create directory
+        if not path.exists(self.username):
+            mkdir(self.username)
 
     def archive(self):
-        self.tweet_manager.archive()
-        self.media_manager.archive()
+        t = tweet(self.username)
+        t.archive()
+        m = media(self.username)
+        m.archive()
